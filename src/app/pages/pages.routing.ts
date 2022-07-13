9ae4fc10f2cbcb4ffe444df8423fd76b79d98e8d
Katya Vergaray <katyavergaray@gmail.com>
2022-07-12 20:11:22 -0500

    ups

--- src/app/pages/pages.routing.ts
+++ src/app/pages/pages.routing.ts
@@ -60,7 +60,8 @@ const routes: Routes = [
 
 @NgModule({
     imports: [RouterModule.forChild(routes)],
-    exports: [RouterModule]
+    exports: [RouterModule],
+    providers:[AuthGuardService]
 })
 
 export class PagesRoutingModule { }
